5f2294b192706e1a1844a740efaa77dc6caa2709
Sasha Holt <sasha23@vt.edu>
2021-11-20 13:40:58 -0500

    Adding/Removing Pronouns Working

--- gender_bot.py
+++ gender_bot.py
@@ -2,6 +2,7 @@ import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.utils import get
 
 load_dotenv()
 
@@ -22,9 +23,29 @@ async def help(ctx):
     await ctx.send("Here's the list of commands!")
     pass
 
-@bot.command(name = 'pronouns')
-async def setPronouns(ctx, arg):
-    await ctx.send("You have been given the role!")
+@bot.command(name = 'setpronouns')
+async def setPronouns(ctx, arg1):
+    guild = ctx.guild
+    user = ctx.message.author
+    if get(ctx.guild.roles, name=arg1):
+        role = discord.utils.get(ctx.guild.roles, name=arg1)
+    else:
+        await ctx.guild.create_role(name=arg1, colour=discord.Colour(0x0062ff))
+        role = discord.utils.get(ctx.guild.roles, name=arg1)
+    await user.add_roles(role)
+    await ctx.send("You have been given the role {}!".format(arg1))
+    pass
+
+@bot.command(name = 'removepronouns')
+async def removePronouns(ctx, arg1):
+    guild = ctx.guild
+    user = ctx.message.author
+    if get(ctx.guild.roles, name=arg1):
+        role = discord.utils.get(ctx.guild.roles, name=arg1)
+        await user.remove_roles(role)
+        await ctx.send("The {} role has been removed!".format(arg1))
+    else:
+        await ctx.send("You do not have the {} role!".format(arg1))
     pass
 
 bot.run(os.getenv("DISCORD_TOKEN"))
